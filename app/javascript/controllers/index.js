585e8b828345322decc5180168be9806eb904932
Uysim <uysimty@gmail.com>
2025-07-07 20:06:28 +0200

    remove manual load controlers

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,10 +1,11 @@
 // Import and register all your controllers from the importmap via controllers/**/*_controller
 import { application } from "./application"
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
+
 eagerLoadControllersFrom("controllers", application)
 
-import HelloController from "./hello_controller"
-application.register("hello", HelloController)
+// import HelloController from "./hello_controller"
+// application.register("hello", HelloController)
 
-import FileUploadController from "./file_upload_controller"
-application.register("file-upload", FileUploadController)
+// import FileUploadController from "./file_upload_controller"
+// application.register("file-upload", FileUploadController)
