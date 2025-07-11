6d02a9c8ee292229bfe45abde301522cb443a689
Uysim <uysimty@gmail.com>
2025-07-11 20:20:29 +0200

    fix controllers pin

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,11 +1,11 @@
 // Import and register all your controllers from the importmap via controllers/**/*_controller
-import { application } from "./application"
+import { application } from "controllers/application"
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 
 eagerLoadControllersFrom("controllers", application)
 
-// import HelloController from "./hello_controller"
-// application.register("hello", HelloController)
+import HelloController from "controllers/hello_controller"
+application.register("hello", HelloController)
 
-// import FileUploadController from "./file_upload_controller"
-// application.register("file-upload", FileUploadController)
+import FileUploadController from "controllers/file_upload_controller"
+application.register("file-upload", FileUploadController)
