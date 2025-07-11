3af49aa95fcb3fcef053f8f616888f90f3a28913
Uysim <uysimty@gmail.com>
2025-07-11 20:26:02 +0200

    fix duplicated import

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -3,9 +3,3 @@ import { application } from "controllers/application"
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 
 eagerLoadControllersFrom("controllers", application)
-
-import HelloController from "controllers/hello_controller"
-application.register("hello", HelloController)
-
-import FileUploadController from "controllers/file_upload_controller"
-application.register("file-upload", FileUploadController)
